4b039d6e474b1acca9d53559f015ea89362acfd7
Abdessamadelbaz <abdo49elbaz@gmail.com>
2023-06-15 15:08:29 -0700

    5️⃣

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
 
 int main(void)
 {
-	char message[] = "Programming is like building a multilingual puzzle";
-
-	puts(message);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
