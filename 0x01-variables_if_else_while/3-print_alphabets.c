f0272f1bd9ae04326067e55f07268a5e25fd4e1f
Abdessamadelbaz <abdo49elbaz@gmail.com>
2023-06-18 05:17:20 -0700

    Task3️⃣

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char alphabet;
+    char upper;
+
+    alphabet = 'a';
+
+    while (alphabet <= 'z')
+    {
+        putchar(alphabet);
+        alphabet++;
+    }
+
+    upper = 'A';
+
+    while (upper <= 'Z')
+    {
+        putchar(upper);
+        upper++;
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
