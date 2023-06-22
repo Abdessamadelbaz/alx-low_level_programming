3d004866eda1c4b7fd2435fe753cc29389f8d7b8
Abdessamadelbaz <abdo49elbaz@gmail.com>
2023-06-21 17:33:27 -0700

    advanced 3️⃣❗❗ ❗✔

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+/**
+ * ft_comb2 - Prints all possible combinations of two two-digit numbers
+ */
+void ft_comb2(void)
+{
+	int i = 0, j = 0;
+
+	while (i <= 99)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	ft_comb2();
+	return (0);
+}
